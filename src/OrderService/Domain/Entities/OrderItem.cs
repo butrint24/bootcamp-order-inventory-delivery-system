105858e415b41d9c9c6e7754360cc1c45aeec4d5
Altin-Millaku <150141135+Altin-Millaku@users.noreply.github.com>
2025-10-15 11:25:46 +0200

    Update OrderItem.cs
    fixed

--- src/OrderService/Domain/Entities/OrderItem.cs
+++ src/OrderService/Domain/Entities/OrderItem.cs
@@ -1,14 +1,31 @@
 ﻿namespace OrderService.Domain.Entities;
+
 public class OrderItem
 {
-    public Guid OrderItemId { get; private set; } = Guid.NewGuid();
-    public Guid OrderId { get; private set; }
-    public Guid ProductId { get; private set; }
-    public int Quantity { get; private set; }
-    public decimal UnitPrice { get; private set; }
+    public Guid OrderItemId { get; set; } = Guid.NewGuid();
+
+    public Guid OrderId { get; set; }
+
+    public Guid ProductId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
     public decimal LineTotal => UnitPrice * Quantity;
 
-    private OrderItem() { }
-    public OrderItem(Guid orderId, Guid productId, int qty, decimal unitPrice)
-    { OrderId = orderId; ProductId = productId; Quantity = qty; UnitPrice = unitPrice; }
+    public OrderItem() { }
+
+    public OrderItem(
+        Guid orderId,
+        Guid productId,
+        int qty,
+        decimal unitPrice
+    )
+    {
+        OrderId = orderId;
+        ProductId = productId;
+        Quantity = qty;
+        UnitPrice = unitPrice;
+    }
 }
