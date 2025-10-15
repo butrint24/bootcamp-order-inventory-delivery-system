f4b4a93e7bebdb12bb7ad68ff13740070ddb01d6
Ylli <183634939+Ylli-Berisha@users.noreply.github.com>
2025-10-15 14:09:32 +0200

    refactor: restructure database initialization script to better match design UML

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,29 +1,25 @@
--- Delivery
+CREATE DATABASE user_db;
+\c user_db
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
+CREATE TYPE role_type AS ENUM (
+  'USER',
+  'ADMIN'
+);
 
-CREATE TYPE delivery_status AS ENUM (
-  'PENDING',
-  'PROCESSING',
-  'DELIVERED'
+CREATE TABLE "user" (
+  user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name VARCHAR(50),
+  surname VARCHAR(50),
+  date_of_birth DATE,
+  tel VARCHAR(50),
+  address TEXT,
+  role role_type
 );
 
 
-CREATE TABLE IF NOT EXISTS delivery (
-  delivery_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  status delivery_status,
-  eta TIMESTAMP,
-  order_id UUID,   
-  user_id UUID     
-);
-
-INSERT INTO delivery (status, eta, order_id, user_id)
-VALUES ('PENDING', now() + interval '3 days', gen_random_uuid(), gen_random_uuid());
-
-
-
--- Order
-
+CREATE DATABASE order_db;
+\c order_db
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TYPE order_status AS ENUM (
@@ -32,30 +28,44 @@ CREATE TYPE order_status AS ENUM (
   'CANCELLED',
   'COMPLETED',
   'CONFIRMED'
-);
+  );
 
-CREATE TABLE IF NOT EXISTS "order" (
+CREATE TABLE "order" (
   order_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   price NUMERIC(12,2),
   status order_status,
   address TEXT,
-  user_id UUID  
+  user_id UUID
 );
-CREATE TABLE IF NOT EXISTS order_item (
+
+CREATE TABLE order_item (
   order_item_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  order_id UUID,     
-  product_id UUID,   
+  order_id UUID,
+  product_id UUID,
   quantity INT
 );
 
-INSERT INTO "order" (price, status, address, user_id)
-VALUES (99.99, 'PENDING', 'Rr. Prishtina', gen_random_uuid());
 
+CREATE DATABASE delivery_db;
+\c delivery_db
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
+CREATE TYPE delivery_status AS ENUM (
+  'PENDING',
+  'PROCESSING',
+  'DELIVERED'
+  );
 
+CREATE TABLE delivery (
+  delivery_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  status delivery_status,
+  eta TIMESTAMP,
+  order_id UUID,
+  user_id UUID
+);
 
--- Invetory
-
+CREATE DATABASE inventory_db;
+\c inventory_db
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TYPE product_category AS ENUM (
@@ -67,32 +77,11 @@ CREATE TYPE product_category AS ENUM (
   'ENTERTAINMENT'
 );
 
-CREATE TABLE IF NOT EXISTS product (
+CREATE TABLE product (
   product_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name TEXT NOT NULL,
   stock INT DEFAULT 0,
   origin TEXT,
   category product_category,
   price NUMERIC(12,2)
-);
-
-INSERT INTO product (name, stock, origin, category, price)
-VALUES ('Phone X', 25, 'China', 'ELECTRONICS', 499.99);
-
-
--- Auth
-
-
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
-CREATE TYPE role_type AS ENUM ('USER', 'ADMIN');
-
-CREATE TABLE IF NOT EXISTS "user" (
-  user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  name VARCHAR(100),
-  surname VARCHAR(100),
-  date_of_birth DATE,
-  tel VARCHAR(50),
-  address TEXT,
-  role role_type
 );
