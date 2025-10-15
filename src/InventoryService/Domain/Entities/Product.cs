a1cf5ecf6464f57436c2a4a638576cecbcf76f8e
Altin-Millaku <150141135+Altin-Millaku@users.noreply.github.com>
2025-10-15 11:19:24 +0200

    Update Product.cs
    fixed

--- src/InventoryService/Domain/Entities/Product.cs
+++ src/InventoryService/Domain/Entities/Product.cs
@@ -1,20 +1,55 @@
-﻿using InventoryService.Domain.Enums;
+using InventoryService.Domain.Enums;
 
 namespace InventoryService.Domain.Entities;
+
 public class Product
 {
     public Guid ProductId { get; private set; } = Guid.NewGuid();
-    public string Name { get; private set; } = null!;
-    public int Stock { get; private set; }
-    public string Origin { get; private set; } = null!;
-    public Category Category { get; private set; }
-    public decimal Price { get; private set; }
+
+    public string Name { get; set; } = null!;
+
+    public int Stock { get; set; }
+
+    public string Origin { get; set; } = null!;
+
+    public Category Category { get; set; }
+
+    public decimal Price { get; set; }
 
     private Product() { }
-    public Product(string name, int stock, string origin, Category category, decimal price)
-    { Name = name; Stock = stock; Origin = origin; Category = category; Price = price; }
 
-    public bool CanReserve(int qty) => qty > 0 && Stock >= qty;
-    public void Reserve(int qty) { if (!CanReserve(qty)) throw new InvalidOperationException("Not enough stock"); Stock -= qty; }
-    public void Release(int qty) { if (qty > 0) Stock += qty; }
+    public Product(
+        string name,
+        int stock,
+        string origin,
+        Category category,
+        decimal price
+    )
+    {
+        Name = name;
+        Stock = stock;
+        Origin = origin;
+        Category = category;
+        Price = price;
+    }
+
+    public bool CanReserve(int qty)
+        => qty > 0 && Stock >= qty;
+
+    public void Reserve(int qty)
+    {
+        if (!CanReserve(qty))
+            throw new InvalidOperationException("Not enough stock");
+
+        Stock -= qty;
+    }
+
+    public void Release(int qty)
+    {
+        if (qty > 0)
+            Stock += qty;
+    }
 }
+
+
+   
