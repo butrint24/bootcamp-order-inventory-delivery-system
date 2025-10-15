d5f6ac04dfd1914f1d02fd542aeb7c1573883546
Altin-Millaku <150141135+Altin-Millaku@users.noreply.github.com>
2025-10-15 11:27:37 +0200

    Update Order.cs

--- src/OrderService/Domain/Entities/Order.cs
+++ src/OrderService/Domain/Entities/Order.cs
@@ -1,32 +1,53 @@
 ﻿using OrderService.Domain.Enums;
 
 namespace OrderService.Domain.Entities;
+
 public class Order
 {
-    public Guid OrderId { get; private set; } = Guid.NewGuid();
-    public decimal Price { get; private set; }           // total price persistuar
-    public OrderStatus Status { get; private set; } = OrderStatus.PENDING;
-    public string Address { get; private set; } = null!;
-    public Guid User_Id { get; private set; }
+    public Guid OrderId { get; set; } = Guid.NewGuid();
 
-    private readonly List<OrderItem> _items = new();
-    public IReadOnlyCollection<OrderItem> Items => _items.AsReadOnly();
+    public decimal Price { get; set; }
 
-    private Order() { }
-    public Order(Guid userId, string address)
-    { User_Id = userId; Address = address; }
+    public OrderStatus Status { get; set; } = OrderStatus.PENDING;
 
-    public void AddItem(Guid productId, int qty, decimal unitPrice)
+    public string Address { get; set; } = null!;
+
+    public Guid User_Id { get; set; }
+
+    public List<OrderItem> Items { get; set; } = new();
+
+    public Order() { }
+
+    public Order(
+        Guid userId,
+        string address
+    )
     {
-        if (qty <= 0) throw new ArgumentOutOfRangeException(nameof(qty));
-        _items.Add(new OrderItem(OrderId, productId, qty, unitPrice));
+        User_Id = userId;
+        Address = address;
+    }
+
+    public void AddItem(
+        Guid productId,
+        int qty,
+        decimal unitPrice
+    )
+    {
+        if (qty <= 0)
+            throw new ArgumentOutOfRangeException(nameof(qty));
+
+        Items.Add(new OrderItem(OrderId, productId, qty, unitPrice));
         Recalculate();
     }
 
     public void MarkProcessing() => Status = OrderStatus.PROCESSING;
+
     public void MarkCompleted() => Status = OrderStatus.COMPLETED;
+
     public void MarkCancelled() => Status = OrderStatus.CANCELLED;
+
     public void Confirm() => Status = OrderStatus.CONFIRMED;
 
-    private void Recalculate() => Price = _items.Sum(i => i.LineTotal);
+    private void Recalculate()
+        => Price = Items.Sum(i => i.LineTotal);
 }
