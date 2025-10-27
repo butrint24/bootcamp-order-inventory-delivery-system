9df0c97f083efa8d39abdd46b8e397b15802c1b3
meriton <137052060+meriton41@users.noreply.github.com>
2025-10-27 14:23:10 +0100

    Init.sql fixed.

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,49 +1,7 @@
- CREATE DATABASE user_db;
- \c user_db
- CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE DATABASE user_db;
+\c user_db
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
- CREATE TYPE role_type AS ENUM (
-   'USER',
-   'ADMIN'
- );
-
- CREATE TABLE "user" (
-   user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-   name VARCHAR(50),
-   surname VARCHAR(50),
-   date_of_birth DATE,
-   tel VARCHAR(50),
-   address TEXT,
-   role role_type
- );
-
-
- CREATE DATABASE order_db;
- \c order_db
- CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
- CREATE TYPE order_status AS ENUM (
-   'PENDING',
-   'PROCESSING',
-   'CANCELLED',
-   'COMPLETED',
-   'CONFIRMED'
-   );
-
- CREATE TABLE "order" (
-   order_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-   price NUMERIC(12,2),
-   status order_status,
-   address TEXT,
-   user_id UUID
- );
-
- CREATE TABLE order_item (
-   order_item_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-   order_id UUID,
-   product_id UUID,
-   quantity INT
- );
 CREATE TABLE "user" (
   user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   name VARCHAR(50) NOT NULL,
@@ -87,8 +45,7 @@ CREATE TABLE order_item (
   quantity INT
 );
 
-
- CREATE DATABASE delivery_db;
+CREATE DATABASE delivery_db;
  \c delivery_db
  CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
@@ -103,24 +60,16 @@ CREATE TABLE order_item (
    user_id UUID
  );
 
- CREATE DATABASE inventory_db;
- \c inventory_db
- CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE DATABASE inventory_db;
+\c inventory_db
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
- CREATE TYPE product_category AS ENUM (
-   'ELECTRONICS',
-   'CLOTHING',
-   'HEALTH_AND_BEAUTY',
-   'ACCESSORIES',
-   'FURNITURE',
-   'ENTERTAINMENT'
- );
-
- CREATE TABLE product (
-   product_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-   name TEXT NOT NULL,
-   stock INT DEFAULT 0,
-   origin TEXT,
-   category product_category,
-   price NUMERIC(12,2)
+CREATE TABLE product (
+  product_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL,
+  stock INT DEFAULT 0,
+  origin TEXT,
+  category TEXT NOT NULL DEFAULT 'OTHER',
+  price NUMERIC(12,2),
+  is_active BOOLEAN DEFAULT TRUE
 );
